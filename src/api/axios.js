310dbfb34e70d0dadf0f3548f7c5fcc1eddc3114
allanbaque1510 <56411730+allanbaque1510@users.noreply.github.com>
2023-09-02 10:39:18 -0500

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instancia = axios.create({
-    baseURL:'https://gestionbackend-api.up.railway.app/',
+    baseURL:'https://gestionbackend-api.up.railway.app/api',
     withCredentials:true
 })
 export default instancia
